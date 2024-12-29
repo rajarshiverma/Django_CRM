8f0b1b96892dd4bb9d0a5af10fc09ecf9fc6f465
rajarshiverma <rajverma9719@gmail.com>
2024-12-29 22:13:46 +0530

    added registration

--- postdb.py
+++ postdb.py
@@ -1,14 +1,16 @@
-import psycopg2
+#Uncomment below code to create database
+# 
+#  import psycopg2
 
-dataBase = psycopg2.connect(
-    host="localhost",
-    user='postgres',
-    password='admin',
-)
-dataBase.set_session(autocommit=True)
+# dataBase = psycopg2.connect(
+#     host="localhost",
+#     user='postgres',
+#     password='admin',
+# )
+# dataBase.set_session(autocommit=True)
 
-cursorObject = dataBase.cursor()
-cursorObject.execute("CREATE DATABASE demo_crm")
-print("Database created")
+# cursorObject = dataBase.cursor()
+# cursorObject.execute("CREATE DATABASE demo_crm")
+# print("Database created")
 
-dataBase.close()
+# dataBase.close()
